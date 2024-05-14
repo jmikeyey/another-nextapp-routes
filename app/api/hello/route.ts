2d09dd6dc5140c179801df1ae170d84abb319fca
Mayki <aw8hurtz@gmail.com>
2024-05-14 13:10:15 +0800

    feaat: adding a cookie

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,3 +1,16 @@
-export async function GET() {
-  return new Response("Hello world")
+import { type NextRequest } from "next/server"
+
+
+export async function GET(request: Request) {
+  const reqHeaders = new Headers(request.headers)
+
+  return new Response(
+    `<h1>${reqHeaders.get("Authorization")?.split(" ")[1]}</h1>`,
+    {
+      headers: {
+        "Content-Type": "text/html",
+        "Set-Cookie": "theme=dark"
+      },
+    }
+  );
 }
